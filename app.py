bcffcf7f369d513cfa4b3ae9c0d070bad4233aa7
yw2423 <yw2423@cornell.edu>
2021-05-09 01:13:07 -0400

    add style

--- app.py
+++ app.py
@@ -3,24 +3,8 @@ from flask import Flask, render_template,request
 import json
 import os
 from azure.cosmos import exceptions, CosmosClient, PartitionKey
-
-from flask_table import Table, Col
 from forms import SubmitForm
 
-# Declare your table
-class ItemTable(Table):
-    name = Col('Time')
-    activity=Col('Activity')
-    temp=Col('temperature')
-    description = Col('Label')
-
-# Get some objects
-class Item(object):
-    def __init__(self, name, description,act,temp):
-        self.name = name
-        self.description = description
-        self.activity = act
-        self.temp = temp
 
 
 
@@ -58,10 +42,10 @@ def main_page():
         result = request.form
         id = result['cowID']
         items = search(id)
-        table = ItemTable(items)
     else:
-        table = ItemTable([])
-    return render_template('display.html', form=form, Table=table)
+        items = []
+
+    return render_template('display.html', form=form, items=items)
 
 
 def search(id):
@@ -69,11 +53,7 @@ def search(id):
     items = list(container.query_items(query=query,enable_cross_partition_query=True))
     print(query)
     print(items)
-    t=[]
-    for item in items:
-        t.append(Item(item['data'][0]['time'], item['label'], item['data'][0]["animal_activity"],
-        item['data'][0]["temp_without_drink_cycles"]))
-    return t
+    return items
 
 
 # @app.route("/table")
